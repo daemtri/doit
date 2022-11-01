256b0c9dc6a11acece7694b5ad6d7b18b46aae58
duanquanyong <duanquanyong@bianfeng.com>
2022-11-01 10:53:46 +0800

    fix command

--- go.mod
+++ go.mod
@@ -7,4 +7,10 @@ require (
 	github.com/valyala/fasttemplate v1.2.2
 )
 
-require github.com/valyala/bytebufferpool v1.0.0 // indirect
+require (
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/term v0.1.0 // indirect
+	mvdan.cc/sh/v3 v3.5.1 // indirect
+)
--- sh.go
+++ sh.go
@@ -2,36 +2,46 @@ package doit
 
 import (
 	"bytes"
-	"github.com/valyala/fasttemplate"
+	"context"
 	"io"
 	"log"
 	"os"
 	"strings"
 	_ "unsafe"
+
+	"github.com/valyala/fasttemplate"
 )
 
 func Run(cmd string) {
 	cmd = fasttemplate.New(cmd, "{", "}").ExecuteFuncString(tagFunc)
 	log.Println("run: ", cmd)
-	exe := ParseCommand(cmd)
-	exe.Stdin = os.Stdin
-	exe.Stdout = os.Stdout
-	exe.Stderr = os.Stderr
-	exe.Env = os.Environ()
-	if err := exe.Run(); err != nil {
+	err := RunCommand(context.Background(), &RunCommandOptions{
+		Command: cmd,
+		Stdin:   os.Stdin,
+		Stdout:  os.Stdout,
+		Stderr:  os.Stderr,
+		Env:     os.Environ(),
+	})
+	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func RunString(cmd string) string {
+	var data bytes.Buffer
 	cmd = fasttemplate.New(cmd, "{", "}").ExecuteFuncString(tagFunc)
 	log.Println("run: ", cmd)
-	exe := ParseCommand(cmd)
-	data, err := exe.CombinedOutput()
+	err := RunCommand(context.Background(), &RunCommandOptions{
+		Command: cmd,
+		Stdin:   os.Stdin,
+		Stdout:  &data,
+		Stderr:  os.Stderr,
+		Env:     os.Environ(),
+	})
 	if err != nil {
 		log.Fatalln(err)
 	}
-	return string(bytes.TrimSpace(data))
+	return string(bytes.TrimSpace(data.Bytes()))
 }
 
 //go:linkname keepUnknownTagFunc github.com/valyala/fasttemplate.keepUnknownTagFunc
